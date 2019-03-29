303defedebd656a51847325a6f2ecf9b67a303d9
Mona Lisa <mona.lisa@gmail.com>
2019-03-28 20:08:11 -0400

    prerelease v0.0.2

--- scripts/github-release-variables.sh
+++ scripts/github-release-variables.sh
@@ -6,5 +6,4 @@ source ~/.private/github/cjsteel
 export GIT_ACCOUNT=cjsteel
 export GIT_SHORT_NAME=model
 export GIT_REPOSITORY=ansible-role-$GIT_SHORT_NAME
-# export GIT_TAG="0.0.1"
 export GIT_TAG=`cat VERSION`
